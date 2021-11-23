83861d9b0b94f686b097d0283278bd3c693022ca
LoreBene99 <lorebene@live.it>
2021-11-23 16:47:58 +0000

    update

--- master.c
+++ master.c
@@ -9,11 +9,13 @@
 #include <string.h>
 #include <sys/wait.h>
 
+/* We create each child process that will execute the processes instead of the father process. */
 
 int spawn(const char * program, char ** arg_list) {
   pid_t child_pid = fork();
-  if (child_pid != 0)
+  if (child_pid != 0){
     return child_pid;
+  }
 
   else {
     execvp (program, arg_list);
@@ -22,26 +24,79 @@ int spawn(const char * program, char ** arg_list) {
   }
 }
 
-
-void build_fifo (const char * name){ 
-    if(mkfifo(name, 0666)==-1){
-        if (errno != EEXIST){
-          perror("There is an error in the creation of the named pipe\n");
-          exit (1);
-        }
-    }
-}
-
 int main(){
-	pid_t pid_comm, pid_m_x, pid_m_z, pid_insp, pid_wd;
-	char pid_motor_x[20], pid_motor_z[20], pid_command[20];
+
+	pid_t pid_comm;
+	pid_t pid_m_x;
+	pid_t pid_m_z; 
+	pid_t pid_insp; 
+	pid_t pid_wd;
 	
-	char * arg_list_m_x[] = { "./motor_x", "fd_comm_to_m_x", NULL };
-	char * arg_list_m_z[] = { "./motor_z", "fd_comm_to_m_z", NULL };
+	char pid_motor_x[20];
+	char pid_motor_z[20];
+	
+	char * arg_list_m_x[] = { "./m_x", "fd_comm_to_m_x" , NULL };
+	char * arg_list_m_z[] = { "./m_z", "fd_comm_to_m_z" , NULL };
 	char * arg_list_comm[] = { "/usr/bin/konsole",  "-e", "./command", (char*)NULL };
 	
-	pid_m_x = spawn("./motor_x", arg_list_m_x); 
-	pid_m_z = spawn("./motor_z", arg_list_m_z);
+	pid_m_x = spawn("./m_x", arg_list_m_x); 
+	pid_m_z = spawn("./m_z", arg_list_m_z);
 	pid_command = spawn("/usr/bin/konsole", arg_list_comm);
 	
+	sprintf(pid_motor_x, "%d", pid_m_x);
+	sprintf(pid_motor_z, "%d", pid_m_z);
+	sprintf(pid_command, "%d", pid_comm);
+
+	char * arg_list_insp[] = { "/usr/bin/konsole",  "-e", "./inspection", pid_motor_x, pid_motor_z, (char*)NULL };
+	pid_inspection = spawn("/usr/bin/konsole", arg_list_insp);
+	char * arg_list_watchdog[] = {"./watchdog", NULL, NULL };
+	pid_watchdog = spawn("./watchdog", arg_list_watchdog);
+
+	char *fd_comm_to_m_x = "/tmp/x";
+	char *fd_comm_to_m_z = "/tmp/z";
+	char *fd_to_insp_x = "/tmp/inspx";
+	char *fd_to_insp_z = "/tmp/inspz";
+
+	mkfifo(fd_comm_to_m_x, 0666);
+	if(mkfifo(fd_comm_to_m_x, 0666)==-1){
+       if (errno != EEXIST){
+         perror("Error creating named fifo\n");
+         exit (1);
+       }
+    } 
+	mkfifo(fd_comm_to_m_z, 0666);
+	if(mkfifo(fd_comm_to_m_z, 0666)==-1){
+       if (errno != EEXIST){
+         perror("Error creating named fifo\n");
+         exit (1);
+       }
+    } 
+          
+	mkfifo(fd_to_insp_x, 0666);
+	if(mkfifo(d_to_insp_x, 0666)==-1){
+       if (errno != EEXIST){
+         perror("Error creating named fifo\n");
+         exit (1);
+       }
+    } 
+    
+	mkfifo(fd_to_insp_z, 0666);
+	if(mkfifo(fd_to_insp_z, 0666)==-1){
+       if (errno != EEXIST){
+         perror("Error creating named fifo\n");
+         exit (1);
+       }
+    } 
+
+	wait(NULL);
+
+	fflush(stdout);
+	unlink("fd_comm_to_m_x");
+	unlink("fd_comm_to_m_z");
+	unlink("fd_to_insp_x");
+	unlink("fd_to_insp_z");
 	
+	return 0;
+
+}
+		
--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 gcc command.c -o command
 gcc m_x.c -o m_x 
-gcc m_z.c -o m_z 
+gcc m_z.c -o m_z
 gcc inspection.c -o inspection
-konsole -e ./command &
-konsole -e ./m_x &
-konsole -e ./m_z &
-konsole -e ./inspection &
-
-
+gcc watchdog.c -o watchdog
 
