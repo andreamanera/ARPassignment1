863cd44de1f3079f223e90d28a380d6dc56b4557
andreamanera <andreamanera.103@gmail.com>
2021-12-01 13:45:07 +0100

    update

---
+++ help.sh
@@ -0,0 +1,7 @@
+more -d info.txt
+more -d ./sources/fmaster/master.txt
+more -d ./sources/fcommand/command.txt
+more -d ./sources/finspection/inspection.txt
+more -d ./sources/fmotor_x/m_x.txt
+more -d ./sources/fmotor_z/m_z.txt
+more -d ./sources/fwatchdog/watchdog.txt
